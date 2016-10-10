db9c21d9d895d5c66bf4ba0eca509a617569c297
Kristian H. Kristensen <hoegsberg@gmail.com>
2016-10-09 21:55:54 -0700

    Don't stub-warn for SF_R32G32B32A32_FLOAT
    We actually do support this one.

--- eu.c
+++ eu.c
@@ -1258,6 +1258,7 @@ builder_emit_sfid_sampler(struct builder *bld, struct inst *inst)
 	bool tex_valid = get_surface(bld->binding_table_address, surface, &args->tex);
 	ksim_assert(tex_valid);
 	switch (args->tex.format) {
+	case SF_R32G32B32A32_FLOAT:
 	case SF_R8G8B8X8_UNORM:
 	case SF_R8G8B8A8_UNORM:
 		break;
