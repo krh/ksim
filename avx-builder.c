607eb6b9ce97b55b7ca0bf137aac05f6c7d2c0b8
Kristian H. Kristensen <hoegsberg@gmail.com>
2016-10-24 21:40:31 -0700

    Add unit test for AVX builder
    We test that we generate the right code by parsing back the disassembly.

--- avx-builder.c
+++ avx-builder.c
@@ -22,6 +22,7 @@
  */
 
 #include <stddef.h>
+#include <string.h>
 
 #include "ksim.h"
 #include "avx-builder.h"
@@ -129,3 +130,167 @@ builder_disasm(struct builder *bld)
 		return false;
 	}
 }
+
+#ifdef TEST_AVX_BUILDER
+
+void
+check_reg_imm_emit_function(const char *fmt,
+			    void (*func)(struct builder *bld, int reg, int imm), int delta)
+{
+	const int imm = 100;
+	struct builder bld;
+
+	for (int reg = 0; reg < 16; reg++) {
+		int count, actual_reg, actual_imm;
+
+		reset_shader_pool();
+		builder_init(&bld, 0, 0);
+
+		func(&bld, reg, imm);
+		builder_disasm(&bld);
+
+		count = sscanf(bld.disasm_output, fmt, &actual_reg, &actual_imm);
+
+		if (count != 2 || reg != actual_reg || imm - delta != actual_imm) {
+			const int size = bld.p - bld.shader->code;
+			printf("fmt='%s' reg=%d imm=%d:\n    ", fmt, reg, imm);
+			for (int i = 0; i < size; i++)
+				printf("%02x ", bld.shader->code[i]);
+			printf("%s\n", bld.disasm_output);
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+void
+check_binop_emit_function(const char *name,
+			  void (*func)(struct builder *bld, int reg, int imm))
+{
+	struct builder bld;
+
+	for (int dst = 0; dst < 16; dst++) {
+		for (int src = 0; src < 16; src++) {
+			int count, actual_dst, actual_src;
+			char actual_name[16];
+
+			reset_shader_pool();
+			builder_init(&bld, 0, 0);
+
+			func(&bld, dst, src);
+			builder_disasm(&bld);
+
+			count = sscanf(bld.disasm_output,
+				       " %16s %%ymm%d,%%ymm%d",
+				       actual_name, &actual_src, &actual_dst);
+
+			if (count != 3 || strcmp(name, actual_name) != 0 ||
+			    dst != actual_dst || src != actual_src) {
+				const int size = bld.p - bld.shader->code;
+				printf("%s dst=%d src=%d:\n    ", name, dst, src);
+				for (int i = 0; i < size; i++)
+					printf("%02x ", bld.shader->code[i]);
+				printf("%s", bld.disasm_output);
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+}
+
+void
+check_triop_emit_function(const char *name,
+			  void (*func)(struct builder *bld, int dst, int src0, int src1))
+{
+	struct builder bld;
+
+	for (int dst = 0; dst < 16; dst++)
+		for (int src0 = 0; src0 < 16; src0++)
+			for (int src1 = 0; src1 < 16; src1++) {
+				int count, actual_dst, actual_src0, actual_src1;
+				char actual_name[16];
+
+				reset_shader_pool();
+				builder_init(&bld, 0, 0);
+
+				func(&bld, dst, src0, src1);
+				builder_disasm(&bld);
+
+				count = sscanf(bld.disasm_output,
+					       "%16s %%ymm%d,%%ymm%d,%%ymm%d",
+					       actual_name, &actual_src0, &actual_src1, &actual_dst);
+
+				if (count != 4 || strcmp(name, actual_name) != 0 ||
+				    dst != actual_dst || src0 != actual_src0 || src1 != actual_src1) {
+
+					const int size = bld.p - bld.shader->code;
+
+					printf("%s dst=%d src0=%d src1=%d:\n    ",
+					       name, dst, src0, src1);
+					for (int i = 0; i < size; i++)
+						printf("%02x ", bld.shader->code[i]);
+					printf("%s", bld.disasm_output);
+
+					exit(EXIT_FAILURE);
+				}
+			}
+}
+
+int main(int argc, char *argv[])
+{
+	check_reg_imm_emit_function("vpbroadcastd 0x%2$x(%%rip),%%ymm%1$d",
+				    builder_emit_vpbroadcastd_rip_relative, 9);
+
+	check_reg_imm_emit_function("vmovdqa 0x%2$x(%%rdi),%%ymm%1$d",
+				    builder_emit_m256i_load, 0);
+	check_reg_imm_emit_function("vmovdqa %%ymm%1$d,0x%2$x(%%rdi)",
+				    builder_emit_m256i_store, 0);
+	check_reg_imm_emit_function("vmovdqa 0x%2$x(%%rdi),%%xmm%1$d",
+				    builder_emit_m128i_load, 0);
+	check_reg_imm_emit_function("vmovdqa %%xmm%1$d,0x%2$x(%%rdi)",
+				    builder_emit_m128i_store, 0);
+	check_reg_imm_emit_function("vpbroadcastd 0x%2$x(%%rdi),%%ymm%1$d",
+				    builder_emit_vpbroadcastd, 0);
+
+	check_reg_imm_emit_function("vmovdqa 0x%2$x(%%rip),%%ymm%1$d",
+				    builder_emit_m256i_load_rip_relative, 8);
+
+	check_triop_emit_function("vpaddd", builder_emit_vpaddd);
+	check_triop_emit_function("vpsubd", builder_emit_vpsubd);
+	check_triop_emit_function("vpmulld", builder_emit_vpmulld);
+	check_triop_emit_function("vaddps", builder_emit_vaddps);
+	check_triop_emit_function("vmulps", builder_emit_vmulps);
+	check_triop_emit_function("vdivps", builder_emit_vdivps);
+	check_triop_emit_function("vsubps", builder_emit_vsubps);
+	check_triop_emit_function("vpand", builder_emit_vpand);
+	check_triop_emit_function("vpxor", builder_emit_vpxor);
+	check_triop_emit_function("vpor", builder_emit_vpor);
+	check_triop_emit_function("vpsrlvd", builder_emit_vpsrlvd);
+	check_triop_emit_function("vpsravd", builder_emit_vpsravd);
+	check_triop_emit_function("vpsllvd", builder_emit_vpsllvd);
+	check_triop_emit_function("vfmadd132ps", builder_emit_vfmadd132ps);
+	check_triop_emit_function("vfmadd231ps", builder_emit_vfmadd231ps);
+
+	check_binop_emit_function("vpabsd", builder_emit_vpabsd);
+	check_binop_emit_function("vrsqrtps", builder_emit_vrsqrtps);
+	check_binop_emit_function("vsqrtps", builder_emit_vsqrtps);
+	check_binop_emit_function("vrcpps", builder_emit_vrcpps);
+
+	/* check_triop_emit_function("vcmpps", builder_emit_vcmpps); */
+
+	check_triop_emit_function("vmaxps", builder_emit_vmaxps);
+	check_triop_emit_function("vminps", builder_emit_vminps);
+
+	/* builder_emit_vpblendvb */
+
+#if 0
+	/* xmm regs */
+	check_triop_emit_function("vpackssdw", builder_emit_vpackssdw);
+	check_binop_emit_function("vpmovsxwd", builder_emit_vpmovsxwd);
+	check_binop_emit_function("vpmovzxwd", builder_emit_vpmovzxwd);
+#endif
+
+	/* builder_emit_vextractf128 */
+
+	return EXIT_SUCCESS;
+}
+
+#endif
