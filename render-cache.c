528263d7f629c30c1f2756d7dd8eac916f912f70
Kristian H. Kristensen <hoegsberg@gmail.com>
2016-11-18 21:15:39 -0800

    Simplify R8_UINT packing a bit

--- render-cache.c
+++ render-cache.c
@@ -342,8 +342,9 @@ sfid_render_cache_rt_write_simd8_r_uint8_ymajor(struct thread *t,
 
 	__m256i r32 = _mm256_permute4x64_epi64(src[0].ireg, SWIZZLE(0, 2, 1, 3));
 
+	__m128i lo = _mm256_extractf128_si256(r32, 0);
 	__m128i hi = _mm256_extractf128_si256(r32, 1);
-	__m128i r16 = _mm_packus_epi32(_mm256_castsi256_si128(r32), hi);
+	__m128i r16 = _mm_packus_epi32(lo, hi);
 	__m128i r8 = _mm_packus_epi16(r16, r16);
 
 	/* FIXME: Needs masking. */
